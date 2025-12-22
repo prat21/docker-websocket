7d51ab78bf5eb2fadb66707b34688861ed1011bb
pratikmukherjee <prmukherjee.21@gmail.com>
2025-12-22 23:17:10 +0530

    Initial comit

---
+++ src/main/java/com/demo/socket/SocketApp.java
@@ -0,0 +1,11 @@
+package com.demo.socket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SocketApp.class,args);
+    }
+}
---
+++ src/main/java/com/demo/socket/config/WebSocketConfig.java
@@ -0,0 +1,23 @@
+package com.demo.socket.config;
+
+import com.demo.socket.handler.SocketHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(getSocketHandler(), "/test")
+                .setAllowedOrigins("*");
+    }
+
+    public WebSocketHandler getSocketHandler() {
+        return new SocketHandler();
+    }
+
+}
