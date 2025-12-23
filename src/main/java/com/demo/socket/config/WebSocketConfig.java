ef266aa34e9ad67a58ee3f48176a6bf1700f9a4f
pratikmukherjee <prmukherjee.21@gmail.com>
2025-12-23 21:20:15 +0530

    Added Okta Oauth authentication

--- src/main/java/com/demo/socket/config/WebSocketConfig.java
+++ src/main/java/com/demo/socket/config/WebSocketConfig.java
@@ -7,14 +7,15 @@ import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(getSocketHandler(), "/test")
-                .addInterceptors(new CustomHandShakeInterceptor())
+        registry.addHandler(getSocketHandler(), "/ws/test")
+                //.addInterceptors(new HttpSessionHandshakeInterceptor(), new CustomHandShakeInterceptor())
                 .setAllowedOrigins("*");
     }
 
