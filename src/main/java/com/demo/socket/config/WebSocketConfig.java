ec98fcd8712cf5d614eaae6f96555272747eeae3
pratikmukherjee <prmukherjee.21@gmail.com>
2025-12-23 22:42:49 +0530

    Updated

--- src/main/java/com/demo/socket/config/WebSocketConfig.java
+++ src/main/java/com/demo/socket/config/WebSocketConfig.java
@@ -1,13 +1,11 @@
 package com.demo.socket.config;
 
 import com.demo.socket.handler.SocketHandler;
-import com.demo.socket.interceptor.CustomHandShakeInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 @Configuration
 @EnableWebSocket
@@ -15,7 +13,6 @@ public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(getSocketHandler(), "/ws/test")
-                //.addInterceptors(new HttpSessionHandshakeInterceptor(), new CustomHandShakeInterceptor())
                 .setAllowedOrigins("*");
     }
 
