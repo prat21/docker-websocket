e03a18511450c55a7f417594a56fdf0232ab2d65
pratikmukherjee <prmukherjee.21@gmail.com>
2025-12-30 00:40:48 +0530

    updated

--- src/main/java/com/demo/socket/config/WebSocketConfig.java
+++ src/main/java/com/demo/socket/config/WebSocketConfig.java
@@ -12,7 +12,7 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(getSocketHandler(), "/ws/socket")
+        registry.addHandler(getSocketHandler(), "/test/socket")
                 .setAllowedOrigins("*");
     }
 
